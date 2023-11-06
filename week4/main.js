b78ae2e76c008d3a74dc63e8325a1ea806df6b77
iam2001Irsa <145094028+iam2001Irsa@users.noreply.github.com>
2023-11-06 19:18:19 +0400

    Update main.js

--- week4/main.js
+++ week4/main.js
@@ -1,6 +1,9 @@
-var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'cities.json');
-ourRequest.onload = function() {
-console.log(ourRequest.responseText);
-};
-ourRequest.send();
+
+  var ourRequest = new XMLHttpRequest();
+  ourRequest.open('GET', 'https://salma-s-hw.github.io/F28WP-lab/cities1.json');
+  ourRequest.onload = function() {
+    console.log(ourRequest.responseText);
+
+  };
+  ourRequest.send();
+  
