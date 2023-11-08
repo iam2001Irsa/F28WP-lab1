65ef4448c6eb33863bb839cf153636ed9224f52a
iam2001Irsa <145094028+iam2001Irsa@users.noreply.github.com>
2023-11-08 23:01:02 +0400

    done

--- week4/app.js
+++ week4/app.js
@@ -1,6 +1,3 @@
-//api key
-//2feb2faa779d431d88bea60f91e2b3dc
-
 const apiKey = "2feb2faa779d431d88bea60f91e2b3dc";
 const button = document.getElementById("btn");
 const weatherdiv = document.getElementById("weather-info");
@@ -11,27 +8,28 @@ button.addEventListener("click", event => {
     var input = document.getElementById('cityInput').value;
     if (!input) {
         alert("Please enter a city name")
+        return;
     }
 
-        var ourRequest = new XMLHttpRequest();
-        ourRequest.open('GET', "https://api.openweathermap.org/data/2.5/weather?q=" + input + "&units=metric&appid=" + apiKey);
-        ourRequest.onload = function () {
-            var ourData = JSON.parse(ourRequest.responseText);
+    var ourRequest = new XMLHttpRequest();
+    ourRequest.open('GET', "https://api.openweathermap.org/data/2.5/weather?q=" + input + "&units=metric&appid=" + apiKey);
+    ourRequest.onload = function () {
+        var ourData = JSON.parse(ourRequest.responseText);
 
-            if (ourRequest.status === 200) {
-                var weatherdiscription = ourData.weather[0].description;
-                var temp = ourData.main.temp;
-                var wind = ourData.wind.speed
-                weatherdiv.innerHTML += "<p>The weather in " + input + " is " +  weatherdiscription + ".<br/> The temperature is " + temp + "°C with a wind speed of " + wind + "m/s</p>";
-            }else{
-            weatherdiv.innerHTML = "Error: Unable to fetch data";
-            }
+        if (ourRequest.status === 200) {
+            var weatherdiscription = ourData.weather[0].description;
+            var temp = ourData.main.temp;
+            var wind = ourData.wind.speed
+            weatherdiv.innerHTML += "<p>The weather in " + input + " is " + weatherdiscription + ".<br/> The temperature is " + temp + "°C with a wind speed of " + wind + "m/s<hr></p>";
         }
-
-            ourRequest.onerror = function () {
-                weatherdiv.innerHTML = "Connection failed"
-            };
-
-            ourRequest.send();
+        else {
+            alert("Error: info not found");
+        }
     }
-)
+
+    ourRequest.onerror = function () {
+        console.log("Connection failed");
+    }
+
+    ourRequest.send();
+})
