ba6a21d4fe80e854063bbd7c7208064c33cee018
iam2001Irsa <145094028+iam2001Irsa@users.noreply.github.com>
2023-11-23 18:10:54 +0400

    ..

--- week5/controllers/auth.js
+++ week5/controllers/auth.js
@@ -48,45 +48,45 @@ exports.register = (req, res) => {
 };
 
 exports.login = async (req, res) => {
-    try {
-        const { email, password } = req.body;
+    try{
+        const {email, password} = req.body;
 
-        if (!email || !password) {
+        if(!email || !password){
             return res.status(400).render('login', {
-                message: 'Please provide both email and password'
+                message: 'Please provide an email and password'
             })
         }
 
         db.query('SELECT * FROM users WHERE email = ?', [email], async (error, results) => {
             console.log(results);
-        
-            if (!results || !(await bcrypt.compare(password, results[0].password))) {
-                return res.status(401).render('login', {
-                    message: 'Incorrect email or password'
-                });
-            } else {
+            if(!results || !(await bcrypt.compare(password, results[0].password))){
+                res.status(401).render('login', {
+                    message: 'Email or password is incorrect'
+                })
+            }else{
                 const id = results[0].id;
 
                 const token = jwt.sign({ id }, process.env.JWT_SECRET, {
                     expiresIn: process.env.JWT_EXPIRES_IN
                 });
 
-                console.log("the token is:" + token);
+                console.log("The token is: " + token);
 
                 const cookieOptions = {
-                    expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000),
-                    httpOnly: true
-                };
+                    expires: new DataTransfer(
+                        Data.now() + process.env.JWT_COOKIE_EXPIRES *24*60*60*1000
+                    ),
+                    httponly: true
+                }
 
                 res.cookie('jwt', token, cookieOptions);
                 res.status(200).redirect("/profile");
             }
         })
-    } catch (error) {
+    }catch(error){
         console.log(error);
     }
-};
-
+}
 
 exports.logout = async (req, res) => {
     res.cookie('jwt', 'logout', {
--- week5/index.js
+++ week5/index.js
@@ -33,7 +33,7 @@ app.use(express.static(publicDirectory));
 //using handlebars as view engine for rendering templates
 app.set('view engine', 'hbs');
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 
 app.use('/', require('./routes/pages'));
